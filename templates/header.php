d61325ccdfb75e2e7df534529e4036a30779c982
Mohammed Gomaa <promgom3a@gmail.com>
2019-03-12 11:45:09 +0200

    fix header responsivness problem

--- templates/header.php
+++ templates/header.php
@@ -23,47 +23,51 @@
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <a class="navbar-brand" href="#">ITI Cafetria</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+       
         <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Products</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Users</a>
-                </li>   
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Manual Order</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Checks</a>
-                </li>
-            </ul>
-            <div class="user-profile">
-                <ul class="nav navbar-nav navbar-right profile-pic" style="padding-left: 68%">
-                    <li>
-                        <div class="inset">
-                            <img src="http://rs775.pbsrc.com/albums/yy35/PhoenyxStar/link-1.jpg~c200">
-                        </div>
+            <div>
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Products</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Users</a>
+                    </li>   
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Manual Order</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Checks</a>
                     </li>
                 </ul>
+            </div>
+           
+        </div>
+        <div class="user-profile"  style="float: right">
+            <ul class="nav navbar-nav navbar-right profile-pic">
+                <li>
+                    <div class="inset">
+                        <img src="http://rs775.pbsrc.com/albums/yy35/PhoenyxStar/link-1.jpg~c200">
+                    </div>
+                </li>
+            </ul>
 
-                <div class="pull-right " style="padding-left: 2%">
-                    <ul class="nav pull-right user-name" >
-                        <li class="dropdown navbar-text"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Welcome, User <b class="caret"></b></a>
-                            <ul class="dropdown-menu">
-                                <li><a href="/auth/logout"><i class="icon-off"></i> Logout</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                </div>
+            <div class="pull-right " style="padding-left: 2%">
+                <ul class="nav pull-right user-name" >
+                    <li class="dropdown navbar-text"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Welcome, User <b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="/auth/logout"><i class="icon-off"></i> Logout</a></li>
+                        </ul>
+                    </li>
+                </ul>
             </div>
         </div>
+         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
     </nav>
 <!-- </body>
 
